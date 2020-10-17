a986d258c04794fae9a49269c2b2196ca356b957
Maktoobgar <mahmoodh1378@gmail.com>
2020-10-17 18:01:59 +0330

    adding a hello world

--- 1_bit_full_adder.v
+++ 1_bit_full_adder.v
@@ -1,12 +1,20 @@
 module fulladder(i_1, i_2, i_c, o_s, o_c);
+
     input i_1, i_2, i_c;
     output o_s, o_c;
     wire w_1, w_2, w_3;
+
+    initial begin
+        $display ("Hello World");
+    end
 
     assign w_1 = i_1 ^ i_2;
     assign o_s = w_1 ^ i_c;
     assign w_2 = w_1 & i_c;
     assign w_3 = i_1 & i_2;
     assign o_c = w_2 | w_3;
+
+    always @*
+        $display("%b", o_c);
 
 endmodule
