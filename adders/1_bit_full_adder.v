22bdca1dd5ee0f2a32e03f0043ff2f4eacdd5094
Maktoobgar <mahmoodh1378@gmail.com>
2020-10-30 17:26:03 +0330

    test-bench for 1-bit-adder

--- adders/1_bit_full_adder.v
+++ adders/1_bit_full_adder.v
@@ -1,20 +1,13 @@
-module fulladder(i_1, i_2, i_c, o_s, o_c);
+module FullAdder_1Bit(i_1, i_2, i_c, o_s, o_c);
 
     input i_1, i_2, i_c;
     output o_s, o_c;
     wire w_1, w_2, w_3;
-
-    initial begin
-        $display ("Hello World");
-    end
 
     assign w_1 = i_1 ^ i_2;
     assign o_s = w_1 ^ i_c;
     assign w_2 = w_1 & i_c;
     assign w_3 = i_1 & i_2;
     assign o_c = w_2 | w_3;
-
-    always @*
-        $display("%b", o_c);
 
 endmodule
---
+++ adders/1_bit_full_adder_tb.v
@@ -0,0 +1,41 @@
+module FullAdder_1Bit_TB;
+
+    reg i_1;
+    reg i_2;
+    reg i_c;
+
+    wire o_s;
+    wire o_c;
+
+    FullAdder_1Bit uut (.i_1(i_1), .i_2(i_2), .i_c(i_c), .o_s(o_s), .o_c(o_c));
+
+        initial begin
+            
+            $dumpfile("test.vcd");
+            $dumpvars(0, FullAdder_1Bit_TB);
+
+            i_1 = 1'b0;
+            i_2 = 1'b0;
+            i_c = 1'b0;
+
+            #10
+            i_1 = 1'b1;
+
+            #10
+            i_c = 1'b1;
+
+            #10
+            i_2 = 1'b1;
+
+            #10
+            $finish;
+
+        end
+
+        initial begin
+
+            $monitor("At time %2t: i_1 = %d, i_2 = %d, i_c = %d, o_c = %d, o_s = %d", $time, i_1, i_2, i_c, o_c, o_s);
+
+        end
+
+endmodule
